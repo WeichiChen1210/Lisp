4904a34c8bc61caeb151def4c516d4e5c066f1e0
WeichiChen1210 <iphone31302@gmail.com>
2019-05-02 15:31:29 +0800

    Add some comments

--- HW2/hw2_q1.py
+++ HW2/hw2_q1.py
@@ -38,20 +38,28 @@ while 1:
 # print(count)
 year_dict = {}
 print("[ Author: " + author + " ]")
+# for each article found
 for r in result:
+    # split the string to get announced year
     year = r.split("originally announced</span>")[1].split("</p>")[0].strip()
     y = year.split(' ')[1].split('.')[0]
 
+    # check if the author is in the list
     if y not in year_dict:
+        # if not, add to dict
         year_dict[y] = 1
     else:
+        # if yes, update
         year_dict[y] += 1
 
+# get the x and y data
 years = []
 times = []
 for key, values in year_dict.items():
     years.append(key)
     times.append(values)
 
+# plot with years(x), times(y) 
 plt.bar(years, times)
+# show the plot
 plt.show()
